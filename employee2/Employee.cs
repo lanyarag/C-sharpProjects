59bad550172477fff9e0918a15b2b86cceccf968
Lany Aragão <lanyaragao5@gmail.com>
2022-07-02 11:45:50 -0300

    Project #4 - Calculating salary, taxes and increases

---
+++ employee2/Employee.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Employee2
+{
+    public class Employee
+    {
+        string Name;
+        double GrossSalary;
+        double Salary;
+        double Tax;
+
+        public void ReadData()
+        {
+            Console.WriteLine("What's your name?");
+            Name = Console.ReadLine();
+            Console.WriteLine("How much is your salary?");
+            GrossSalary = double.Parse(Console.ReadLine());
+            Console.WriteLine("How much do you pay in tax?");
+            Tax = double.Parse(Console.ReadLine()); 
+            Salary = GrossSalary - Tax;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"{Name}'s salary is {Salary}");
+        }
+
+        public void SalaryIncrease()
+        {
+            Console.WriteLine("How much will the salary increase (%)?");
+            double Increase = double.Parse(Console.ReadLine());
+            double Increase2 = Increase / 100;
+            Salary = Salary + (GrossSalary * Increase2);
+            Console.WriteLine($"With the {Increase}% increase, the salary will be {Salary}");
+        }
+    }
+}
---
+++ employee2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Employee2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee();
+            employee.ReadData();
+            employee.PrintData();
+            employee.SalaryIncrease();
+        }
+    }
+}
