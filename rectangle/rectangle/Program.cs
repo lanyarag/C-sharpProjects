a2d0fd3d5b1ee4c0ee2baf1e9870f83ce5fddf76
Lany Aragão <lanyaragao5@gmail.com>
2022-07-02 11:06:13 -0300

    Project #3 - Rectangle

---
+++ rectangle/rectangle/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace rectangle
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.ReadData();
+            rectangle.CalcArea();
+            rectangle.CalcPerimeter();
+            rectangle.CalcDiagonal();
+        }
+    }
+}
---
+++ rectangle/rectangle/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿
+namespace rectangle
+{
+    public class Rectangle
+    {
+        double Width { get; set; }
+        double Height { get; set; }
+
+        public void ReadData()
+        {
+            Console.WriteLine("Put the data of the rectangle.");
+            Console.WriteLine("Width: ");
+            Width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Heigth: ");
+            Height = double.Parse(Console.ReadLine());
+        }
+
+        public void CalcArea()
+        {
+            double Area = Width * Height;
+            Console.WriteLine($"A rectangle with height {Height} e width {Width} has area {Area}");
+        }
+
+        public void CalcPerimeter()
+        {
+            double Perimeter = 2*(Width + Height);
+            Console.WriteLine($"A rectangle with height {Height} e width {Width} has perimeter {Perimeter}");
+        }
+
+        public void CalcDiagonal()
+        {
+            double Diagonal;
+            Diagonal = Math.Sqrt(Math.Pow(Width, 2) + Math.Pow(Height, 2));
+            Console.WriteLine($"A rectangle with height {Height} e width {Width} has diagonal {Diagonal}");
+        }
+    }
+}
