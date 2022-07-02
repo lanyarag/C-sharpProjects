4392c0212f9cd6d76fd2e8de0b506e0c384d5735
Lany Aragão <lanyaragao5@gmail.com>
2022-07-02 11:03:15 -0300

    Project #2 - Average salary

---
+++ employees/employees/Employee.cs
@@ -0,0 +1,28 @@
+﻿
+namespace employees
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public double Wage { get; set; }
+
+        public Employee(string name, double wage)
+        {
+            Name = name;
+            Wage = wage;
+        }
+
+        public string CapName()
+        {
+            Console.WriteLine("What's your name?");
+            Name = Console.ReadLine();
+            return Name;
+        }
+        public double CapWage()
+        {
+            Console.WriteLine("What's your salary?");
+            Wage = double.Parse(Console.ReadLine());
+            return Wage;
+        }
+    }
+}
---
+++ employees/employees/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace employees
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Employee employee1 = new Employee("", 0);
+            Employee employee2 = new Employee("", 0);
+
+            employee1.CapName();
+            employee1.CapWage();
+            employee2.CapName();
+            employee2.CapWage();
+
+
+
+            double average = (employee1.Wage + employee2.Wage)/2;
+            
+            Console.WriteLine($"Name: {employee1.Name} | Salary: {employee1.Wage}");
+            Console.WriteLine($"Name: {employee2.Name} | Salary: {employee2.Wage}");
+
+            Console.WriteLine($"The averagy salary is {average}");
+        }
+    }
+}
