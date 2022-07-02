87d9e8d1b32e5f18b8a756b19e4225a8ae8f5156
Lany Aragão <lanyaragao5@gmail.com>
2022-07-02 12:13:40 -0300

    Calculating student grades and checking status

---
+++ Student/Student/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Student
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Student student = new Student();
+
+            student.CaptData();
+            student.CalcAverage();
+            student.Status();
+          
+        }
+    }
+}
---
+++ Student/Student/Student.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Student
+{
+    public class Student
+    {
+        string Name;
+        double Average;
+        double SumScore;
+        
+        public double CaptData()
+        {
+            Console.WriteLine("Welcome to the program. What's your name?");
+            Name = Console.ReadLine();
+            Console.WriteLine("What was the grade for the first quarter?");
+            double Score1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("What was the grade for the second quarter?");
+            double Score2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("What was the grade for the third quarter?");
+            double Score3 = double.Parse(Console.ReadLine());
+            double SumScore = Score1 + Score2 + Score3;
+            return SumScore;
+        }
+        public double CalcAverage()
+        {
+            Average = SumScore / 3;
+            return Average;
+        }
+
+        public void Status()
+        {
+            if (Average > 60)
+            {
+                Console.WriteLine($"The student {Name} is approved. Congratulations!");
+            }
+            else
+            {
+                Console.WriteLine($"The student {Name} was not approved");
+            }
+        }
+
+    }
+}
