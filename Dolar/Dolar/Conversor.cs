9b4d548ed67b252b205a404ac566b3d6c726a997
Lany Aragão <lanyaragao5@gmail.com>
2022-07-02 12:35:52 -0300

    Project #6 - Conversion of purchases from dollars to reais

---
+++ Dolar/Dolar/Conversor.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Dolar
+{
+    public class Conversor
+    {
+        double ValueDollar;
+        double IOF = 0.06;
+
+        public void Exchange()
+        {
+            Console.WriteLine("How much is the dollar today?");
+            ValueDollar = double.Parse(Console.ReadLine());
+            Console.WriteLine("What was the purchase price (in dollar)?");
+            double ValuePurchase = double.Parse(Console.ReadLine());
+            double ValueReais = ValuePurchase * ValueDollar;
+            ValueReais = ValueReais + (ValueReais * IOF);
+            Console.WriteLine($"The purchase price in reais is {ValueReais}");
+        }
+    }
+}
---
+++ Dolar/Dolar/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Dolar
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Conversor conversor = new Conversor();
+
+            conversor.Exchange();
+        }
+    }
+}
