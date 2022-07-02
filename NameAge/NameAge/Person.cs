5dfabfbd70529817b68044605d749420a1e327b9
Lany Aragão <lanyaragao5@gmail.com>
2022-07-02 10:58:39 -0300

    Project #1 - NameAge

---
+++ NameAge/NameAge/Person.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace NameAge
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+        }
+
+        public void CaptureData()
+        {
+            Console.WriteLine("What's your name?");
+            Name = Console.ReadLine();
+
+            int age;
+            Console.WriteLine("How old are you?");
+            Age = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ NameAge/NameAge/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NameAge
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string name;
+            int age;
+
+            Person person1 = new Person("",0);
+            Person person2 = new Person("",0);
+
+            person1.CaptureData();
+            person2.CaptureData();
+
+            if (person1.Age > person2.Age)
+            {
+                Console.WriteLine($"{person1.Name} is older than {person2.Age}");
+            }
+            else if (person1.Age < person2.Age)
+            {
+                Console.WriteLine($"{person2.Name} is older than {person2.Age}");
+            }
+            else
+            {
+                Console.WriteLine($"You are {person1.Age} years old");
+            }
+
+        }
+    }
+}
